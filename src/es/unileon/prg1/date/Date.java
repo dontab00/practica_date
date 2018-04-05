af4d8602444d0b0767691c55046b1e590a3ebe47
dontab00 <dontab00@estudiantes.unileon.es>
2018-04-05 20:38:46 +0200

    Metodos hasta el primer for incluido agregados

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -24,22 +24,72 @@ public class Date {
 		return this.day;
 	}
 
+	public String getMonthName(){
+		String monthsName="";
+		switch (this.month){
+			case 1:
+				monthsName= "January";
+			break;
+			case 2:
+				monthsName= "February";
+			break;
+			case 3:
+				monthsName= "March";
+			break;
+			case 4:
+				monthsName= "April";
+			break;
+			case 5:
+				monthsName= "May";
+			break;
+			case 6:
+				monthsName= "June";
+			break;
+			case 7:
+				monthsName= "July";
+			break;
+			case 8:
+				monthsName= "August";
+			break;
+			case 9:
+				monthsName= "September";
+			break;
+			case 10:
+				monthsName= "October";
+			break;
+			case 11:
+				monthsName= "November";
+			break;
+			case 12:
+				monthsName= "December";
+			break;
+		}
+		return monthsName;
+	}
+
 	boolean isSameYear(Date another){
-		if ( this.year == another.getYear() ){
+		if (this.year == another.getYear()){
 			return true;
 		}
 		return false;
 	}
 
 	boolean isSameMonth(Date another){
-		if ( this.month == another.getMonth() ){
+		if (this.month == another.getMonth()){
 			return true;
 		}
 		return false;
 	}
 	
 	boolean isSameDay(Date another){
-		if ( this.day == another.getDay() ){
+		if (this.day == another.getDay()){
+			return true;
+		}
+		return false;
+	}
+
+	boolean isSame(Date another){
+		if (this.day == another.getDay() && this.month == another.getMonth() && this.year == another.getYear()){
 			return true;
 		}
 		return false;
@@ -49,4 +99,150 @@ public class Date {
 		return this.day + "/" + this.month + "/" + this.year;
 	}
 
+	public boolean isMonthDayOk(){
+		boolean isMonthsDayOk=false;
+			switch (this.month){
+				case 1:
+					if (this.day<0 || this.day>31){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 2:
+					if (this.day<0 || this.day>28){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 3:
+					if (this.day<0 || this.day>31){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 4:
+					if (this.day<0 || this.day>30){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 5:
+					if (this.day<0 || this.day>31){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 6:
+					if (this.day<0 || this.day>30){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 7:
+					if (this.day<0 || this.day>31){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 8:
+					if (this.day<0 || this.day>31){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 9:
+					if (this.day<0 || this.day>30){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 10:
+					if (this.day<0 || this.day>31){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 11:
+					if (this.day<0 || this.day>30){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+				case 12:
+					if (this.day<0 || this.day>31){
+						isMonthsDayOk=false;
+					}
+					else {
+						isMonthsDayOk=true;
+					}
+				break;
+			}
+		return isMonthsDayOk;
+	}
+
+	public String dateSeason(){
+		String season="";
+		switch (this.month){
+			case 1:
+				season= "Winter";
+			case 2:
+				season= "Winter";
+			case 3:
+				season= "Winter";
+			break;
+			case 4:
+				season= "Spring";
+			case 5:
+				season= "Spring";
+			case 6:
+				season= "Spring";
+			break;
+			case 7:
+				season= "Summer";
+			case 8:
+				season= "Summer";
+			case 9:
+				season= "Summer";
+			break;
+			case 10:
+				season= "Autumn";
+			case 11:
+				season= "Autumn";
+			case 12:
+				season= "Autumn";
+			break;
+		}
+		return season;
+	}
+
+	public String monthLeft(){
+		StringBuilder months = new StringBuilder();
+		for (int i=this.month;i<=12;i++){
+			this.month = this.month+1;
+			months.append(this.getMonthName()).append(" ");
+		}
+		return months.toString();
+	}
 }
--- src/es/unileon/prg1/date/MainDate.java
+++ src/es/unileon/prg1/date/MainDate.java
@@ -4,11 +4,16 @@ public class MainDate {
 
 	public static void main(String[] args) {
 		Date today, tomorrow;
-		today = new Date(20, 3, 2018);
-		tomorrow = new Date(21, 3, 2018);
+		today = new Date(3, 4, 2018);
+		tomorrow = new Date(4, 4, 2018);
 		System.out.println(today + " isSameYear " + tomorrow + "? " + today.isSameYear(tomorrow));
 		System.out.println(today + " isSameMonth " + tomorrow + "? " + today.isSameMonth(tomorrow));
 		System.out.println(today + " isSameDay " + tomorrow + "? " + today.isSameDay(tomorrow));
+		System.out.println(today + " isSame " + tomorrow + "? " + today.isSame(tomorrow));
+		System.out.println("Today's month is: " + today.getMonthName());
+		System.out.println("Today's month day is: " + today.isMonthDayOk());
+		System.out.println("Today's season is: " + today.dateSeason());
+		System.out.println("This months left untill year's end: " + today.monthLeft());
 	}
 
 }
